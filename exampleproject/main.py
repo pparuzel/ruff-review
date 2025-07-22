e3db6bd00009e2b263c41cf732b063d427a97366
Pawel Paruzel <pawelparuzel95@gmail.com>
2025-07-22 10:56:58 +0000

    test: Add bad code to test GitHub Actions review

--- exampleproject/main.py
+++ exampleproject/main.py
@@ -1,5 +1,11 @@
 # You can have any logic in this project,
 # stage the changes, and ruff will verify them.
 
+def BadCode() -> int:
+    x = 'helo wrold'
+    if id(x) % 3 == 0:
+        return 42
+    return x
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    print(BadCode())
